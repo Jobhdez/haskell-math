30cb53b8517559a34235254018b5ab02e41d4ecb
Job Henandez Lara <hj93@protonmail.com>
2023-09-02 11:21:04 -0700

    add trace

--- src/LinearAlgebra.hs
+++ src/LinearAlgebra.hs
@@ -22,3 +22,19 @@ computeTwoDDeterminant matrix =
     row2 = matrix!!1
     e = row!!0 * row2!!1
     e2 = row!!1 * row2!!0
+
+trace :: [[Int]] -> Int
+trace matrix =
+  -- expects a square matrix
+  -- Example:
+  --   ghci> trace [[2,1,5],[2,3,4],[0,1,0]]
+  --   5
+  sum (traceHelper matrix 0)
+  
+traceHelper :: [[Int]] -> Int -> [Int]
+traceHelper [] n = []
+traceHelper (x:xs) n =
+  (x!!n):rest
+  where
+    rest = traceHelper xs (n+1)
+    
