f6b4f4005ba6216ea259c7cc27e4d8e402dd3914
Job Henandez Lara <hj93@protonmail.com>
2023-09-05 22:21:09 -0700

    add determinant test

--- test/Test.hs
+++ test/Test.hs
@@ -3,10 +3,13 @@ module Main where
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
+import LinearAlgebra
+
 
 main :: IO ()
 main = hspec $ do
   describe "Prelude.head" $ do
     it "test the determinant of the matrix" $ do
-      3 + 3 `shouldBe` 6
+      determinant [[1,2,3], [45,60,33], [100,45,32]] `shouldBe` -7770
+      
       
