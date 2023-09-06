20cb1de972b7d449506cda7c9d3cd648f878c83c
Job Henandez Lara <hj93@protonmail.com>
2023-09-05 22:24:42 -0700

    add trace tests

--- test/Test.hs
+++ test/Test.hs
@@ -11,5 +11,8 @@ main = hspec $ do
   describe "Prelude.head" $ do
     it "test the determinant of the matrix" $ do
       determinant [[1,2,3], [45,60,33], [100,45,32]] `shouldBe` -7770
+
+    it "test the trace of a square matrix" $ do
+      trace [[2,1,5],[2,3,4],[0,1,0]] `shouldBe` 5
       
       
