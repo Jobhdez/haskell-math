70ef2aa977b29c2002670add79598b3634ac7966
Job Henandez Lara <hj93@protonmail.com>
2023-09-06 13:39:00 -0700

    fix typo

--- test/Test.hs
+++ test/Test.hs
@@ -26,9 +26,14 @@ main = hspec $ do
   describe "Matrix Arith" $ do
     it "test matrix addition" $ do
       add (Matrix [[3,4,5],[5,6,7]])  (Matrix [[1,2,3],[1,1,1]]) `shouldBe` (Matrix [[4,6,8],[6,7,8]])
+    it "test matrix subtraction" $ do
       sub (Matrix [[3,4,5],[5,6,7]])  (Matrix [[1,2,3],[1,1,1]]) `shouldBe` (Matrix [[2,2,2],[4,5,6]])
+    it "test matrix multplication" $ do
       mul (Matrix [[2,3,4],[4,5,6]]) (Matrix [[2,3],[4,5],[6,7]]) `shouldBe` (Matrix [[40,49],[64,79]])
+    it "test power of matrix" $ do
       pow (Matrix [[2,3,4],[4,5,6]]) 3 `shouldBe` (Matrix [[8,27,64],[64,125,216]])
+    it "test matrix exponential" $ do
       exponential (Matrix [[2,3,4],[4,5,6]]) `shouldBe` [[7.389056,20.085537,54.59815],[54.59815,148.41316,403.4288]]
+    it "test matrix log" $ do
       matLog (Matrix [[2,3,4],[4,5,6]]) `shouldBe` [[0.6931472,1.0986123,1.3862944],[1.3862944,1.609438,1.7917595]]
 
