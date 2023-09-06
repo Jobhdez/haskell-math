be821c3efa5e63ecffe4182358ad56abc02feaa1
Job Henandez Lara <hj93@protonmail.com>
2023-09-05 22:27:30 -0700

    add upper triangular tests

--- test/Test.hs
+++ test/Test.hs
@@ -14,5 +14,7 @@ main = hspec $ do
 
     it "test the trace of a square matrix" $ do
       trace [[2,1,5],[2,3,4],[0,1,0]] `shouldBe` 5
-      
+
+    it "test the uppertriangular of a square matrix" $ do
+      upperTriangular  [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]] `shouldBe` [[1,2,3,4],[0,6,7,8],[0,0,11,12],[0,0,0,16]]
       
