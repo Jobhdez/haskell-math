d0ce6fe709edb88fce57ee014fa7b7bace257e70
Job Henandez Lara <hj93@protonmail.com>
2023-09-16 16:28:52 -0700

    add main (server)

---
+++ src/Main.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Main where
+
+import Prelude ()
+import Prelude.Compat
+
+import Control.Monad.Except
+import Control.Monad.Reader
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Attoparsec.ByteString
+import Data.ByteString (ByteString)
+import Data.List
+import Data.Maybe
+import Data.String.Conversions
+import Data.Time.Calendar
+import GHC.Generics
+import Lucid
+import Network.HTTP.Media ((//), (/:))
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import System.Directory
+import Text.Blaze
+import Text.Blaze.Html.Renderer.Utf8
+import Servant.Types.SourceT (source)
+import qualified Data.Aeson.Parser
+import qualified Text.Blaze.Html
+
+import LinearAlgebra
+
+type API = "compute" :> ReqBody '[JSON] ExprInfo :> Post '[JSON] Int
+
+data ExprInfo = ExprInfo {
+  expr :: [[Int]]
+  } deriving Generic
+
+instance FromJSON ExprInfo
+instance ToJSON ExprInfo
+
+exprForClient :: ExprInfo -> Int
+exprForClient e = expr' where
+  expr' = determinant (expr e)
+
+
+lAlgServer :: Server API
+lAlgServer = compute where
+  compute :: ExprInfo -> Handler Int
+  compute clientInfo = return (exprForClient clientInfo)
+              
+
+userAPI :: Proxy API
+userAPI = Proxy
+
+app1 :: Application
+app1 = serve userAPI lAlgServer
+
+main :: IO ()
+main = run 8081 app1
