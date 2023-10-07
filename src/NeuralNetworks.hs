1f0f830dd08fe940de50f83dfdc57f56029819f4
Job <hj93@protonmail.com>
2023-10-07 15:38:57 -0700

    add softmax2d for ints

--- src/NeuralNetworks.hs
+++ src/NeuralNetworks.hs
@@ -4,6 +4,7 @@ import Matrix
 import Vector 
 
 data Vectorf = Vectorf [Float] deriving (Show, Eq)
+data Matrixf = Matrixf [[Float]] deriving (Show, Eq)
 
 getVector :: Vector -> [Int]
 getVector (Vector vec) = vec
@@ -25,6 +26,16 @@ instance NN Vector where
 instance NN Vectorf where
   softmax = softmaxf'
   logsoftmax = logsoftmaxf'
+
+class NN2D a where
+  softmax2d :: a -> Matrixf
+  logsoftmax2d :: a -> Matrixf
+
+instance NN2D Matrix where
+  softmax2d = softmax2d'
+  logsoftmax2d = logsoftmax2d'
+
+
 {-
 class NN a where
   softmax :: a -> [Float]
@@ -67,15 +78,18 @@ logsoftmax' (Vector vec) =
   in
     (Vectorf fvec)
     
-{-
-softmax2d :: [[Int]] -> [[Float]]
-softmax2d matrix =
-  map (\x -> softmax' x) matrix
+softmax2d' :: Matrix -> Matrixf
+softmax2d' (Matrix matrix) =
+  (Matrixf matf)
+  where
+    matf = map (\x -> (getVectorf (softmax' (Vector x)))) matrix
 
-logsoftmax2d :: [[Int]] -> [[Float]]
-logsoftmax2d matrix =
-  map (\x -> logsoftmax' x) matrix
--}
+logsoftmax2d' :: Matrix -> Matrixf
+logsoftmax2d' (Matrix matrix) =
+  (Matrixf matf)
+  where
+    matf = map (\x -> (getVectorf (logsoftmax' (Vector x)))) matrix
+
 softmaxf' :: Vectorf -> Vectorf
 softmaxf' (Vectorf vec) =
   let expVec = map (\x -> exp x) vec
