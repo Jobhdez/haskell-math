5623a1e57328e2a28a30fb0d5bf3ac5664a426fe
Job Henandez Lara <hj93@protonmail.com>
2023-10-17 12:42:31 -0700

    remove type class

--- src/Fraction.hs
+++ src/Fraction.hs
@@ -1,38 +1,31 @@
 module Fraction where
 
-import ArithTypeClass
-
-instance ArithMathObject Fraction where
-  add = addFractions (+)
-  sub = subFractions (-)
-  mul = mulFractions (*)
-
 data  Fraction = Fraction {
   numerator :: Int,
   denominator :: Int
   } deriving (Show, Eq)
 
-addFractions :: (Int -> Int -> Int) -> (Fraction -> Fraction -> Fraction)
-addFractions fn fraction1 fraction2 =
+addFractions :: Fraction -> Fraction -> Fraction
+addFractions fraction1 fraction2 =
   (Fraction n d)
   where
-    n = fn ((numerator fraction1) * (denominator fraction2)) ((numerator fraction2) * (denominator fraction1))
+    n =  ((numerator fraction1) * (denominator fraction2)) + ((numerator fraction2) * (denominator fraction1))
     d = (denominator fraction1) * (denominator fraction2)
 
 
-subFractions :: (Int -> Int -> Int) -> (Fraction -> Fraction -> Fraction)
-subFractions fn (Fraction num denom) (Fraction num2 denom2) =
+subFractions :: Fraction -> Fraction -> Fraction
+subFractions (Fraction num denom) (Fraction num2 denom2) =
   (Fraction n d)
   where
-    n = fn (num * denom2) (num2 * denom)
+    n = (num * denom2) - (num2 * denom)
     d = denom*denom2
 
-mulFractions :: (Int -> Int -> Int) -> (Fraction -> Fraction -> Fraction)
-mulFractions fn (Fraction num denom) (Fraction num2 denom2) =
+mulFractions :: Fraction -> Fraction -> Fraction
+mulFractions (Fraction num denom) (Fraction num2 denom2) =
   (Fraction n d)
   where
-    n = fn num num2
-    d = fn denom denom2
+    n = num *  num2
+    d = denom * denom2
 
 
    
